ca95061b5da7db5d31a135ef3be032176f807b20
piotr-sneller <piotr@sneller.io>
2023-10-06 19:11:15 +0200

    Added memops stubs. For now use the cstring equivalents, but ready for AVX512 vectorization

---
+++ iguana/memops.h
@@ -0,0 +1,48 @@
+// Copyright 2023 Sneller, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#pragma once
+#include <cstring>
+#include <array>
+#include "common.h"
+
+namespace iguana {
+    namespace memory {
+        namespace internal {
+            inline void zero(void* p, std::size_t n) noexcept {
+                std::memset(p, 0, n); // TODO: optimize me!
+            }
+        }
+
+        template <
+            typename T
+        > inline std::enable_if_t<std::is_trivial_v<T>> zero(T* p, std::size_t n) noexcept {
+            internal::zero(static_cast<void*>(p), n * sizeof(T));
+        }
+
+        template <
+            typename T,
+            std::size_t N
+        > inline void zero(T (&a)[N]) noexcept {
+            zero(&a[0], N);        
+        }
+
+        template <
+            typename T,
+            std::size_t N
+        > inline void zero(std::array<T, N>& a) noexcept {
+            zero(a.data(), N);        
+        }
+    }
+}
