b76534b4b82c6bd91c15250da83291ee81ab2bd3
piotr-sneller <piotr@sneller.io>
2023-10-09 18:39:51 +0200

    Added more methods

--- iguana/buffer.h
+++ iguana/buffer.h
@@ -22,7 +22,14 @@ namespace iguana {
         using container_type = std::vector<std::uint8_t>;
 
     public:
-        using size_type = typename container_type::size_type;
+        using size_type         = typename container_type::size_type;
+        using value_type        = typename container_type::value_type;
+        using pointer           = typename container_type::pointer;
+        using const_pointer     = typename container_type::const_pointer;
+        using reference         = typename container_type::reference;
+        using const_reference   = typename container_type::const_reference;
+        using iterator          = typename container_type::iterator;
+        using const_iterator    = typename container_type::const_iterator;
 
     private:
          container_type m_data;
@@ -36,11 +43,68 @@ namespace iguana {
         byte_buffer& operator =(byte_buffer&&) = default;
 
     public:
+
+        constexpr size_type size() const noexcept {
+            return m_data.size();
+        }
     
+        constexpr const_pointer data() const noexcept {
+            return m_data.data();
+        }
+        
+        constexpr const_reference operator [](size_type k) const noexcept {
+            assert(k < size());
+            return m_data[k];
+        }
+
+        constexpr reference operator [](size_type k) noexcept {
+            assert(k < size());
+            return m_data[k];
+        }
+
+        const_iterator cbegin() const noexcept {
+            return m_data.cbegin();
+        }
+
+        const_iterator cend() const noexcept {
+            return m_data.cend();
+        }
+
+        iterator begin() noexcept {
+            return m_data.begin();
+        }
+
+        const_iterator begin() const noexcept {
+            return m_data.begin();
+        }
+
+        iterator end() noexcept {
+            return m_data.end();
+        }
+
+        const_iterator end() const noexcept {
+            return m_data.cend();
+        }
+
         void reserve(size_type n) {
             m_data.reserve(n);
         }
+
+        void clear() noexcept {
+            m_data.clear();
+        }
         
+        void append(std::uint8_t v) {
+            m_data.push_back(v);
+        }
+
+        void append(const std::uint8_t *p, std::size_t n);
+
+        void append(const byte_buffer& buf) {
+            append(buf.data(), buf.size());
+        }
+
         template <typename T> void append_little_endian(T v);
+        template <typename T> void append_big_endian(T v);
     };
 }
