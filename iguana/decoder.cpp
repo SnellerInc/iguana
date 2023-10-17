ed560ddd8e71a847f78eb0a12b6eb68f7ea3bb80
piotr-sneller <piotr@sneller.io>
2023-10-17 13:37:51 +0200

    Enabled ANS1 decoding

--- iguana/decoder.cpp
+++ iguana/decoder.cpp
@@ -110,28 +110,21 @@ func (d *Decoder) decode(, dst []byte, src []byte) ([]byte, errorCode) {
                     // Decode the compressed content
                     ans32::decoder{}.decode(dst, static_cast<std::size_t>(len_uncompressed), is, ans_tab);
                 }
-      
             } break;
 
 		case command::decode_ans1: {
                 const std::uint64_t len_uncompressed = read_control_var_uint(src, ctrl_cursor);
                 const std::uint64_t len_compressed = read_control_var_uint(src, ctrl_cursor);
 
-                // Recover the ANS decoding table from the input stream                
-                ans::statistics::decoding_table ans_tab;
-                {   input_stream is{src + data_cursor, std::size_t(len_compressed)};
+                 {  ans::statistics::decoding_table ans_tab;
+                    input_stream is{src + data_cursor, std::size_t(len_compressed)};
                     data_cursor += len_compressed;
+                    // Recover the ANS decoding table from the input stream                
                     ans::statistics{is}.build_decoding_table(ans_tab);
+
+                    // Decode the compressed content
+                    ans1::decoder{}.decode(dst, static_cast<std::size_t>(len_uncompressed), is, ans_tab);
                 }
-
-IGUANA_UNIMPLEMENTED
-/*TODO
-			dst, ec = ans1DecodeExplicit(encoded, &d.anstab, int(lenUncompressed), dst)
-			if ec != ecOK {
-				return dst, ec
-			}*/
-
-    			data_cursor += len_compressed;
             } break;
 
 		case command::decode_ans_nibble: {
